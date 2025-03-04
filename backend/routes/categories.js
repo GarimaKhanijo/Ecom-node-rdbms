b23d536395a7391386ee12549d3ae37e8282f2af
Gaurav <gauravluthra928@gmail.com>
2025-03-05 01:19:08 +0530

    bjvxj

--- backend/routes/categories.js
+++ backend/routes/categories.js
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 const express = require("express");
 const { getAllCategories, getCategoryById, createCategory, updateCategory, deleteCategory } = require("../controllers/categoriescon");
 const router = express.Router();
@@ -9,16 +9,4 @@ router.post("/", createCategory);
 router.patch("/", updateCategory);
 router.delete("/:id", deleteCategory);
 
-=======
-const express = require("express");
-const { getAllCategories, getCategoryById, createCategory, updateCategory, deleteCategory } = require("../controllers/categoriescon");
-const router = express.Router();
-
-router.get("/", getAllCategories);
-router.get("/:id", getCategoryById);
-router.post("/", createCategory);
-router.patch("/", updateCategory);
-router.delete("/:id", deleteCategory);
-
->>>>>>> 8d0c3eed3ec16e2fa3b03e78db03ba4456ad4f24
 module.exports = router;
--- backend/routes/products.js
+++ backend/routes/products.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const express = require("express");
 const { getAllProducts, getProductById, createProduct, updateProduct, deleteProduct } = require("../controllers/productscon");
 const router = express.Router();
@@ -9,16 +8,4 @@ router.post("/", createProduct);
 router.patch("/:id", updateProduct);
 router.delete("/:id", deleteProduct);
 
-=======
-const express = require("express");
-const { getAllProducts, getProductById, createProduct, updateProduct, deleteProduct } = require("../controllers/productscon");
-const router = express.Router();
-
-router.get("/", getAllProducts);
-router.get("/:id", getProductById);
-router.post("/", createProduct);
-router.patch("/:id", updateProduct);
-router.delete("/:id", deleteProduct);
-
->>>>>>> 8d0c3eed3ec16e2fa3b03e78db03ba4456ad4f24
 module.exports = router;
