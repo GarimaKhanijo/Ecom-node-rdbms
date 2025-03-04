50d014a05a0522412a59754a1c93521a231b2a60
GauravKumain2111 <122596381+GauravKumain2111@users.noreply.github.com>
2025-03-04 10:24:24 +0530

    server.js

--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,9 @@ const cors = require("cors");
 
 const userRoutes = require("./routes/user");
 const otpRoutes=require("./routes/otp");
-
+const orderRoutes = require("./routes/orders");
+const orderItems = require("./routes/orderitems");
+const payments = require("./routes/payments");
 const app = express();
 app.use(cors());
 app.use(express.json());
@@ -13,6 +15,9 @@ app.use(express.json());
 app.get("/", (req, res) => res.send("<h1>E-Commerce API Running</h1>"));
 app.use("/users", userRoutes);
 app.use("/otp",otpRoutes);
+app.use("/orders",orderRoutes);
+app.use("/order_items",orderItems);
+app.use("/payments",payments);
 
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
