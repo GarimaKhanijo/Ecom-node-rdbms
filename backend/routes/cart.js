19f707977329349c65a7b20a65d9b3e28deceda1
Garima <garima313.be22@chitkara.edu.in>
2025-03-04 11:56:20 +0530

    routes and controllers for cart

---
+++ backend/routes/cart.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const router = express.Router();
+const { getCartItems, addToCart, updateCartItem, removeCartItem, clearCart } = require("../controllers/cartcon");
+const isAuthenticated = require("../middlewares/auth");
+
+router.get("/", isAuthenticated, getCartItems); // Get cart items for logged-in user
+router.post("/", isAuthenticated, addToCart); // Add item to cart
+router.put("/:product_id", isAuthenticated, updateCartItem); 
+router.delete("/:product_id", isAuthenticated, removeCartItem); // Remove item from cart
+router.delete("/", isAuthenticated, clearCart); // Clear cart for logged-in user
+
+module.exports = router;
